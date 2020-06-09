3e1be041a97af211c37b4b9d8df34dd283b1fcfc
Jaden <55096567+JadenIT@users.noreply.github.com>
2020-06-09 17:29:24 +0300

    Updated

--- example/index.js
+++
@@ -1,9 +0,0 @@
-const server = require('../lib/Server')
-const SomeController = require('./SomeController')
-const cors = require('./cors')
-
-server.use('/', cors)
-
-new SomeController('/api')
-
-server.listen(3000)
